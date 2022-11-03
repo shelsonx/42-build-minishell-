fc28c4f5d85740134c59e6f1a5ee58b437e9a7bc
shelsonx <shelsonx@gmail.com>
2022-11-03 09:06:11 -0400

    simple example chdir and getcwd functions

---
+++ learning/simple_example_chdir_getcwd.c
@@ -0,0 +1,31 @@
+#include "../libs/libft/libft.h"
+#include <unistd.h>
+#include <stdio.h>
+
+static void show_current_dir(void)
+{
+    char cwd[256];
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+        perror("getcwd error()");
+    else
+        ft_printf("current dir:\n %s\n", cwd);
+}
+
+void    go_to_dir(char *path)
+{
+    int ch = chdir(path);
+
+    if (ch == 0)
+        ft_printf("Sucessful!\n");
+    else
+        ft_printf("Not sucessful!\n");
+}
+
+int main(void)
+{
+    show_current_dir();
+    go_to_dir("/home/shelson/");
+    show_current_dir();
+    return (0);
+}
