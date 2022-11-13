885048dcf56b5dd4bcb310ff4d6d1dc6c5dfff48
Pedro Silva <progerio@hotmail.com>
2022-11-12 23:03:47 -0300

    Initialization of lexical analyzer

---
+++ sources/lexer/lexical_analyzer.c
@@ -0,0 +1,104 @@
+#include "../../includes/minishell.h"
+
+	char isDigit(char c) 
+    {
+		if (c >= '0' && c <= '9')
+        {
+            printf("print - %c\n", c);
+            return c;
+        } else {
+            return FALSE;
+        }
+	}
+	
+	char isChar(char c) 
+    {
+		if ((c >= 'a' && c <= 'z') || (c>='A' && c <= 'Z'))
+        {
+            printf("print - %c\n", c);
+            return c;
+        } else {
+            return FALSE;
+        }
+	}
+	
+	char isOperator(char c) 
+    {
+		if (c == '=' || c == '!' || c == '+' || c == '-' || c == '*' || c == '/')
+	    {
+            printf("print - %c\n", c);
+            return c;
+        } else {
+            return FALSE;
+        }
+    }
+
+    char isRedirection(char c) 
+    {
+		if (c == '>' || c == '<' || c == '>>' || c == '<<')
+        {
+            printf("print - %c\n", c);
+            return c;
+        } else {
+            return FALSE;
+        }
+    }
+
+    char isPipe(char c) 
+    {
+		if (c == '|'  || c == '|')
+        {
+            printf("print - %c\n", c);
+            return c;
+        } else {
+            return FALSE;
+        }
+    }
+
+    char isQuote(char c) 
+    {
+		if (c == '\''  || c == '\"')
+        {
+            printf("print - %c\n", c);
+            return c;
+        } else {
+            return FALSE;
+        }
+    }
+	
+    char isSpace(char c) 
+    {
+		if (c == '\n' || c== '\r') 
+           {
+                printf("print - %c\n", c);
+                return c;
+            } else {
+                return FALSE;
+            } 
+		if (c == ' ' || c == '\t' || c == '\n' || c == '\r')
+            {
+                printf("print - %c\n", c);
+                return c;
+            } else {
+                return FALSE;
+            }
+	}
+
+    char lexycal_analyzer(char *string)
+    {
+        int i = 0; 
+        while(i <= ft_strlen(string))
+        {
+            isDigit(string[i]);
+            isChar(string[i]);
+            isOperator(string[i]);
+            isRedirection(string[i]);
+            isPipe(string[i]);
+            isQuote(string[i]);
+            isSpace(string[i]);
+            //printf("%d - %c\n",i, text[i]);
+            i++;
+        }
+
+        return 0;
+    }
