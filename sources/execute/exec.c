b01a9e6648c7f9767fda45d99aead26761f228d7
shelsonx <shelsonx@gmail.com>
2022-11-17 13:28:37 -0500

    adjust fds to received fds of the redirections

--- sources/execute/exec.c
+++ sources/execute/exec.c
@@ -1,4 +1,5 @@
 #include "../../includes/minishell.h"
+#include <fcntl.h>
 
 char **create_args(char **pipeline, int pos_cmd, int pos_param)
 {
@@ -39,12 +40,12 @@ pid_t	execute_child_process(t_data *data)
 	return (pid);
 }
 
-void	exec_first_command(t_data *data)
+void	exec_first_command(t_data *data, int fd_in)
 {
 	pid_t	child_pid;
 	int		status;
 
-	data->fd_in = STDIN_FILENO;
+	data->fd_in = fd_in;
 	data->fd_out = data->fds[0][1];
 	data->args = create_args(data->pipeline, 0, 1);
 	child_pid = execute_child_process(data);
@@ -69,17 +70,31 @@ void	exec_middles_commands(t_data *data, int total_cmds_middles)
 	}
 }
 
-void	exec_last_command(t_data *data, int index, int pos_cmd, int pos_param)
+void	exec_last_command(t_data *data, int index, int fd_out, int pos_cmd, int pos_param)
 {
 	pid_t	child_pid;
 	int		status;
 
 	data->fd_in = data->fds[index][0];
-	data->fd_out = STDOUT_FILENO;
+	data->fd_out = fd_out;
 	data->args = create_args(data->pipeline, pos_cmd, pos_param);
 	child_pid = execute_child_process(data);
 	ft_close_fds(data->fds);
     waitpid(child_pid, &status, 0);
+}
+
+void	exec_one_command(t_data *data, int fd_in, int fd_out)
+{
+	pid_t	child_pid;
+	int		status;
+
+	data->fds = create_pipes(1);
+	data->fd_in = fd_in;
+	data->fd_out = fd_out;
+	data->args = create_args(data->pipeline, 0, 1);
+	child_pid = execute_child_process(data);
+	ft_close_fds(data->fds);
+	waitpid(child_pid, &status, 0);
 }
 
 int execute(char *line)
@@ -91,31 +106,26 @@ int execute(char *line)
 	total_commands = ft_len_rows_tab(data.pipeline) /2;
 	if (total_commands == 1)
 	{
-		pid_t	child_pid;
-		int		status;
-
-		data.fds = create_pipes(1);
-		data.fd_in = STDIN_FILENO;
-		data.fd_out = STDOUT_FILENO;
-		data.args = create_args(data.pipeline, 0, 1);
-		child_pid = execute_child_process(&data);
-		ft_close_fds(data.fds);
-		waitpid(child_pid, &status, 0);
+		//int fd_infile = open("infile", O_RDONLY, 0777);
+		//int fd_outfile = open("outile", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		exec_one_command(&data, STDIN_FILENO, STDOUT_FILENO);
 	}
 	if (total_commands == 2)
 	{
+		//int fd_infile = open("infile", O_RDONLY, 0777);
 		data.fds = create_pipes(1);
-		exec_first_command(&data);
-		exec_last_command(&data, total_commands -2, 
-			ft_len_rows_tab(data.pipeline) -2, 
+		exec_first_command(&data, STDIN_FILENO);
+		exec_last_command(&data, total_commands -2, STDOUT_FILENO,
+			ft_len_rows_tab(data.pipeline) -2,
 			ft_len_rows_tab(data.pipeline) -1);
 	}
 	else if (total_commands > 2)
 	{
+		//int fd_infile = open("infile", O_RDONLY, 0777);
 		data.fds = create_pipes(total_commands -1);
-		exec_first_command(&data);
+		exec_first_command(&data, STDIN_FILENO);
 		exec_middles_commands(&data, total_commands -2);
-		exec_last_command(&data, total_commands -2, 
+		exec_last_command(&data, total_commands -2, STDOUT_FILENO, 
 			ft_len_rows_tab(data.pipeline) -2, 
 			ft_len_rows_tab(data.pipeline) -1);
 	}
