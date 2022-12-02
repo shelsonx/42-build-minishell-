cd9360e54b8a74f64cbfabeac3af389aaa36e276
shelsonx <shelsonx@gmail.com>
2022-12-02 06:58:05 -0500

    add message command not found

--- sources/execute/command.c
+++ sources/execute/command.c
@@ -26,15 +26,34 @@ char	*get_exec_command(char *arg)
 	return (NULL);
 }
 
+void	error_command_msg(char **args, char *input_cmd)
+{
+	char	*msg;
+	char	*tmp;
+
+	if (args[0] == NULL)
+	{
+		tmp = ft_strjoin("minishell: ", input_cmd);
+		msg = ft_strjoin(tmp, ": command not found\n");
+		write(STDOUT_FILENO, msg, ft_strlen(msg));
+		free(tmp);
+		free(msg);
+		free(input_cmd);
+	}
+}
+
 void	exec_one_command(t_data *data, int fd_in, int fd_out)
 {
 	pid_t	child_pid;
 	int		status;
+	char	*input_cmd;
 
 	data->fds = create_pipes(1);
 	data->fd_in = fd_in;
 	data->fd_out = fd_out;
+	input_cmd = ft_strdup(data->pipeline[0]);
 	data->args = create_args(data->pipeline);
+	error_command_msg(data->args, input_cmd);
 	child_pid = execute_child_process(data);
 	ft_close_fds(data->fds);
 	waitpid(child_pid, &status, 0);
@@ -44,10 +63,13 @@ void	exec_first_command(t_data *data, int fd_in)
 {
 	pid_t	child_pid;
 	int		status;
+	char	*input_cmd;
 
 	data->fd_in = fd_in;
 	data->fd_out = data->fds[0][1];
+	input_cmd = ft_strdup(data->pipeline[0]);
 	data->args = create_args(data->pipeline);
+	error_command_msg(data->args, input_cmd);
 	child_pid = execute_child_process(data);
     waitpid(child_pid, &status, 0);
 }
@@ -55,19 +77,19 @@ void	exec_first_command(t_data *data, int fd_in)
 void	exec_middles_commands(t_data *data, t_parser *parser_data, int total_cmds_middles)
 {
 	int		i;
-	int     pos_cmd;
+	char	*input_cmd;
 
 	i = 0;
-	pos_cmd = 2;
 	while (i < total_cmds_middles)
 	{ 
 		data->fd_in = data->fds[i][0];
 		data->fd_out = data->fds[i+1][1];
 		data->pipeline = ft_split(ht_search(parser_data->table, ft_itoa(i+1)), ' ');
+		input_cmd = ft_strdup(data->pipeline[0]);
 		data->args = create_args(data->pipeline);
+		error_command_msg(data->args, input_cmd);
 		execute_child_process(data);
 		i++;
-		pos_cmd += 2;
 	}
 }
 
@@ -75,10 +97,13 @@ void	exec_last_command(t_data *data, int index, int fd_out)
 {
 	pid_t	child_pid;
 	int		status;
+	char	*input_cmd;
 
 	data->fd_in = data->fds[index][0];
 	data->fd_out = fd_out;
+	input_cmd = ft_strdup(data->pipeline[0]);
 	data->args = create_args(data->pipeline);
+	error_command_msg(data->args, input_cmd);
 	child_pid = execute_child_process(data);
 	ft_close_fds(data->fds);
     waitpid(child_pid, &status, 0);
