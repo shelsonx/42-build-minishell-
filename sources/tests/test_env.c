9e0a5c97ab09b051f51b2d3e6fb7ff17c0e6aeb5
shelsonx <shelsonx@gmail.com>
2022-12-06 23:56:26 -0500

    first test_env

---
+++ sources/tests/test_env.c
@@ -0,0 +1,75 @@
+#include "../../includes/minishell.h"
+/*
+compile:
+    cc test_env.c ../utils/ht_functions.c ../utils/ht_utils.c ../utils/linkedlist_utils.c ../../libs/libft/libft.a 
+*/
+
+typedef struct s_builtins
+{
+    int         size;
+    HashTable   *env;
+}   t_builtins;
+
+char	*get_env_path(char *path, t_builtins *builtin)
+{
+	char	*env_path;
+    char    *result_search;
+    char    *num_str;
+	int		i;
+
+	i = 0;
+	while (i < builtin->size)
+	{
+        num_str = ft_itoa(i);
+        result_search = ht_search(builtin->env, num_str);
+		env_path = ft_strnstr(result_search, path, ft_strlen(result_search));
+		if (env_path)
+        {
+            free(num_str);
+			return (ft_substr(env_path, ft_strlen(path)+1, ft_strlen(env_path)));
+        }
+        free(num_str);
+		i++;
+	}
+	return (env_path);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+    t_builtins builtins;
+
+    builtins.env = create_table(2000);
+    builtins.size = 0;
+
+    char    *num_str;
+    while (envp[builtins.size])
+    {
+        num_str = ft_itoa(builtins.size);
+        ht_insert(builtins.env, num_str, envp[builtins.size]);
+        free(num_str);
+        builtins.size++;
+    }
+
+    printf("size env= %d\n", builtins.size);
+    int i = 0;
+    while (i < builtins.size)
+    {
+        num_str = ft_itoa(i);
+        printf("%s\n", ht_search(builtins.env, num_str));
+        free(num_str);
+        i++;
+    }
+    
+    char *var = "PWD";
+    printf("*************************************************\n");
+    printf("TEST GET_ENV %s\n", var);
+    printf("*************************************************\n");
+    
+    char *result;
+    result = get_env_path(var, &builtins);
+    printf("%s\n", result);
+    free(result);
+    free_hashtable(builtins.env);
+
+    return (0);
+}
